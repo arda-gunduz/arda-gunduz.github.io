9d21c4f8a6068400640b28cc3f118520d8e5a0ac
Arda-Gunduz <arda.gunduz@etu.unilim.fr>
2025-03-19 11:02:07 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arda-gunduz.github.io',
-  base: 'portfolio',
+  base: '/',
   integrations: [mdx(), sitemap()],
 
   vite: {
